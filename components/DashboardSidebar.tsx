951b878f299e492e5115da755b4ca125c1466dc0
kb603 <kojoaddobaffour@gmail.com>
2024-12-15 19:18:52 +0000

    dashboard layout ✅

--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -1,15 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <aside className="bg-gray-800 text-white w-64 min-h-screen flex flex-col">
-      <div className="p-4 text-lg font-bold">Dashboard</div>
+    <div className="text-white w-64 min-h-screen flex flex-col bg-white border-r shadow-lg">
+      <div className="p-4 text-lg font-bold">
+        <Image src="/images/logo.svg" width={120} height={85} alt="logo" />
+      </div>
       <nav className="flex-grow">
         <ul className="space-y-2 p-4">
           <li>
             <Link
               href="/overview"
-              className="block p-2 rounded hover:bg-gray-700"
+              className="block p-2 rounded hover:bg-gray-700 transition-colors duration-300"
             >
               Overview
             </Link>
@@ -17,7 +20,7 @@ export default function Sidebar() {
           <li>
             <Link
               href="/orders"
-              className="block p-2 rounded hover:bg-gray-700"
+              className="block p-2 rounded hover:bg-gray-700 transition-colors duration-300"
             >
               Orders
             </Link>
@@ -25,7 +28,7 @@ export default function Sidebar() {
           <li>
             <Link
               href="/schedule"
-              className="block p-2 rounded hover:bg-gray-700"
+              className="block p-2 rounded hover:bg-gray-700 transition-colors duration-300"
             >
               Schedules
             </Link>
@@ -33,13 +36,13 @@ export default function Sidebar() {
           <li>
             <Link
               href="/payment"
-              className="block p-2 rounded hover:bg-gray-700"
+              className="block p-2 rounded hover:bg-gray-700 transition-colors duration-300"
             >
               Payments
             </Link>
           </li>
         </ul>
       </nav>
-    </aside>
+    </div>
   );
 }
